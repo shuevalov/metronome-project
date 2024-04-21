3e9c0eea6a89b2aa45daa3a96631131eca84df88
danya shuvalov <shuevalov@gmail.com>
2024-04-21 23:13:29 +0300

    nav graph added

--- app/src/main/java/ru/shuevalov/metronome_project/MainActivity.kt
+++ app/src/main/java/ru/shuevalov/metronome_project/MainActivity.kt
@@ -2,12 +2,24 @@ package ru.shuevalov.metronome_project
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import ru.shuevalov.metronome_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().add(R.id.main_frag_container, MainMetronomeFragment()).commit()
+//        supportFragmentManager.beginTransaction().add(R.id.main_frag_container, MainMetronomeFragment()).commit()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_frag_container) as NavHostFragment
+        navController = navHostFragment.navController
+        val navGraph = navController.navInflater.inflate(R.navigation.main_nav_graph)
+
     }
 }
