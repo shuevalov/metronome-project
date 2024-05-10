deff66cea311d6a99c268d56eea8da71e554f6f6
danya shuvalov <shuevalov@gmail.com>
2024-05-10 18:05:45 +0300

    for 5th module

--- app/src/main/java/ru/shuevalov/metronome_project/fragments/AccountSettingsFragment.kt
+++ app/src/main/java/ru/shuevalov/metronome_project/fragments/AccountSettingsFragment.kt
@@ -2,5 +2,7 @@ package ru.shuevalov.metronome_project.fragments
 
 import androidx.fragment.app.Fragment
 
-class AuthorizationFragment : Fragment() {
+class AccountSettingsFragment : Fragment() {
+
+
 }
--- app/src/main/java/ru/shuevalov/metronome_project/fragments/SettingsFragment.kt
+++ app/src/main/java/ru/shuevalov/metronome_project/fragments/SettingsFragment.kt
@@ -32,5 +32,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.prefs, rootKey)
+
+
     }
 }
